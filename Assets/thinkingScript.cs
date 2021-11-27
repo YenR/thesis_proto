539eed95517437396cacc8c041d74f0781fb160b
YenR <yen.0001@gmail.com>
2021-11-27 15:55:17 +0100

    frog version

---
+++ Assets/thinkingScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thinkingScript : MonoBehaviour
+{
+    public GameObject player_thoughts;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(hudscipt.money == "15")
+            player_thoughts.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        player_thoughts.SetActive(false);
+    }
+}
