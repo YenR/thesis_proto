cbd4fb75b443a3fed8a4b995c5dcb50cc5b61a2b
YenR <yen.0001@gmail.com>
2021-11-27 12:58:03 +0100

    lots of qol updates

---
+++ Assets/musicScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicScript : MonoBehaviour
+{
+    // from https://answers.unity.com/questions/1260393/make-music-continue-playing-through-scenes.html
+
+    public static musicScript instance;
+
+    public AudioSource audioSource;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            return;
+
+        DontDestroyOnLoad(transform.gameObject);
+        PlayMusic();
+    }
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
---
+++ Assets/resetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetScript : MonoBehaviour
+{
+    public void resetStatics()
+    {
+        globalVars.progress = 0f;
+        globalVars.MCx_level = 1;
+        hudscipt.money = "0";
+        hudscipt.text = "Talk to mom";
+        bandit_script.talked_to = false;
+        bandit_script.state = 0;
+        doc_script.friendly = true;
+    }
+}
