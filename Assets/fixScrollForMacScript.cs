a64bd4d3a625019a6a77c3e71c0ab7b8fc55011b
YenR <yen.0001@gmail.com>
2022-03-30 12:29:13 +0200

    work on look and feel of questionnaire (wip)

---
+++ Assets/fixScrollForMacScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fixScrollForMacScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.OSXPlayer || SystemInfo.operatingSystem.Contains("Mac OS X"))
+        {
+            ScrollRect sr = this.gameObject.GetComponent<ScrollRect>();
+            if(sr)
+            {
+                sr.scrollSensitivity = 1;
+            }
+        }
+    }
+    
+}
---
+++ Assets/welcomeOnMobileScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class welcomeOnMobileScript : MonoBehaviour
+{
+    public GameObject mobileWarning;
+    public Animator crossfade;
+
+    private void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            mobileWarning.SetActive(true);
+            //crossfade.SetTrigger("Start");
+            
+        }
+    }
+}
